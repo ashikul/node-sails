35a1a4d23448f6be84d0e22c97bec2e4410daade
ashikul <alam.ashikul@gmail.com>
2017-08-28 03:49:27 -0400

    add working npm directory

---
+++ test.js
@@ -0,0 +1,5 @@
+const electron = require("./submodules/athenapdf/cli/node_modules/electron"); //this works
+// const electron = require("electron"); //doesnt work
+
+console.log('hello');
+console.log(electron);
