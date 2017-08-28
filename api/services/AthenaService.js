b24df9759dc4da2e57c1c71eb85faf6acb5213fb
ashikul <alam.ashikul@gmail.com>
2017-08-28 01:51:53 -0400

    trying out axios, small updates

--- api/services/AthenaService.js
+++ api/services/AthenaService.js
@@ -4,7 +4,7 @@
 module.exports = {
     test: function () {
         console.log('test function called');
-        return false;
+        return false;   
     },
     createPDF: function () {
         console.log('createPDF function called');
