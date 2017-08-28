18d1efaf40a6457e0c1c0138bb9521224070923f
ashikul <alam.ashikul@gmail.com>
2017-08-27 22:15:15 -0400

    new api/model

---
+++ api/controllers/GithubProjectController.js
@@ -0,0 +1,11 @@
+/**
+ * GithubProjectController
+ *
+ * @description :: Server-side logic for managing githubprojects
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
