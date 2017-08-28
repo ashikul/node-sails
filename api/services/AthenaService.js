9a010c190aa1abee52bca1db78adf77ae00291db
ashikul <alam.ashikul@gmail.com>
2017-08-27 22:14:52 -0400

    add services

---
+++ api/services/AthenaService.js
@@ -0,0 +1,13 @@
+// TODO: require athena
+
+
+module.exports = {
+    test: function () {
+        console.log('test function called');
+        return false;
+    },
+    createPDF: function () {
+        console.log('createPDF function called');
+        // TODO:
+    }
+}
---
+++ api/services/GithubService.js
@@ -0,0 +1,13 @@
+module.exports = {
+    test: function () {
+        console.log('test function called');
+        return false;
+    },
+    isThisAValidProject: function (username, repo) {
+        return false;
+    },
+    getSourceCode: function () {
+        console.log('getSourceCode function called');
+        // TODO:
+    }
+}
